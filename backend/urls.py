be6c4c81bb1a3a36a5a564bd28ea67a4eb916b83
Matthew Neba <mattneba4343@gmail.com>
2024-06-28 02:02:46 -0600

    Some more route configuration

--- backend/urls.py
+++ backend/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url="/static/favicon.png", permanent=True)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("comments.urls")),
+    re_path(r"^favicon\.ico$", favicon_view),
 ]
