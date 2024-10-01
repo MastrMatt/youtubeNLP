002e78237605f1a4303f11784041dd5e39814430
Matthew Neba <mattneba4343@gmail.com>
2024-09-30 21:46:25 -0600

    Now Reconfigure CSRF

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,11 @@ SECRET_KEY = "django-insecure-svfbs8ke!chxokb1at4n$j&510m1ri#bjrjr65#de%&)q$r(vl
 DEBUG = True
 
 ALLOWED_HOSTS = ["youtube-nlp.apps.mastrmatt.com", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://youtube-nlp.apps.mastrmatt.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 
 # Application definition
